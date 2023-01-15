5b8ab51aaa114f77c0627840f82dfe4082bd0ce7
Yiorgos <yiorgos.bagakis@protonmail,com>
2023-01-15 10:39:43 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,84 @@
+
+import React, { useState } from 'react';
+import './App.css'
+
+function App() {
+  const [selectedDay, setSelectedDay] = useState(null);
+  const [text, setText] = useState('');
+
+  function handleClick(day, month) {
+    setSelectedDay({ day, month });
+    setText(localStorage.getItem(`day-${day}-${month}`) || '');
+  }
+
+  function handleTextChange(e) {
+    setText(e.target.value);
+  }
+
+  function handleSave() {
+    localStorage.setItem(`day-${selectedDay.day}-${selectedDay.month}`, text);
+  }
+
+  const currentDate = new Date();
+  const currentDay = currentDate.getUTCDate();
+  const currentMonth = currentDate.getUTCMonth();
+  const currentYear = currentDate.getUTCFullYear();
+
+  const months = [
+    "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+    "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+  ];
+  
+  return (
+    <div className='App'>
+         <h1>🌞 Ilia's 2023 Calendar </h1>
+         {selectedDay && (
+        <div>
+          <textarea value={text} onChange={handleTextChange} /><br></br>
+         <button className="save-btn" onClick={handleSave}>Save</button>
+        </div>
+      )}
+      {months.map((month, monthIndex) => {
+        const daysInMonth = new Date(currentYear, monthIndex + 1, 0).getDate();
+        return (
+          <>
+            {Array.from({ length: daysInMonth }, (_, i) => i + 1).map(day => {
+              const isCurrentDay = day === currentDay && monthIndex === currentMonth;
+              const isSelectedDay = selectedDay && selectedDay.day === day && selectedDay.month === monthIndex;
+              return (
+                <button
+                  key={`${month}${day}`}
+                  onClick={() => handleClick(day, monthIndex)}
+                  disabled={day < currentDay && monthIndex <= currentMonth}
+                  style={{
+                    backgroundColor: isCurrentDay ? "#ff770a" : isSelectedDay ? "#000000" : "initial",
+                    color: isCurrentDay ? "initial" : isSelectedDay ? "white" : "initial",
+                  }}
+                >
+                  {`${month}${day}`}
+                </button>
+             )
+            })}
+          </>
+        );
+      })}
+   
+    </div>
+  );
+}
+
+export default App;
+           
+
+   
+      {/* <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p> */}
